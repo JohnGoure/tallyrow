bf9c67e96255a94e927f5e60a69c057127d5b2f6
JohnGoure <johnsgithub@gmail.com>
2018-04-17 16:01:12 -0500

    Changed the url of add-item to additem.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,7 +7,7 @@ export const Menu = () => (
     <nav className='menu'>
         <h3 className="row1 col1 logo"><NavLink to="/" className="logo">TallyRow</NavLink></h3>
         <MenuItem row="row1" col="col1" navlink="stats" title="Stats" /> 
-        <MenuItem row="row1" col="col3" navlink="add-item" title="+" />
+        <MenuItem row="row1" col="col3" navlink="additem" title="+" />
         <MenuItem row="row1" col="col2" navlink="todolist" title="List" />
     </nav>
 )
